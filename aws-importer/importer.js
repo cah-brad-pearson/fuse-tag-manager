d1dab25da1bba63422512c87003b1356ab1c2974
Brad Pearson <brad.pearson@LOH070XX461JG5J.cardinalhealth.net>
2020-08-04 09:18:26 -0400

    updating importer to reenable all the other importers

--- aws-importer/importer.js
+++ aws-importer/importer.js
@@ -10,38 +10,28 @@ AWS.config.update({ region: "us-east-1" });
 
 async function processResources(cb) {
     // EC2 instances
-    // clearAndFetchEc2Instances()
-    //     .then((ec2Instances) => {
-    //         // Populate the DB table
-    //         return writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, ec2Instances);
-    //     })
-    //     .then((ec2RecordsWritten) => {
-    //         console.log(`wrote ${ec2RecordsWritten} EC2 records to db table`);
-    //     })
-    //     // RDS instances
-    //     .then(() => {
-    //         return clearAndFetchRDSRecords();
-    //     })
-
-    // .then((rdsInstances) => {
-    //     return writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, rdsInstances);
-    // })
-    // .then((rdsRecordsWritten) => {
-    //     console.log(`wrote ${rdsRecordsWritten} RDS records to db table`);
-    // })
-    // // Query AWS for EBS volumes
-    // .then(() => {
-    //     getEBSVolumes();
-    // })
-    // clearAndFetchEBSVolumes()
-    //     .then((ebsVolumes) => {
-    //         console.log(`fetched ${ebsVolumes.length} EBS records`);
-    //         return writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, ebsVolumes);
-    //     })
-    //     .then((recordsWritten) => {
-    //         console.log(`wrote ${recordsWritten} EBS records to db table`);
-    //     })
-    clearAndFetchS3Records()
+    clearAndFetchEc2Instances()
+        // Populate the DB table
+        .then((ec2Instances) => writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, ec2Instances))
+        .then((ec2RecordsWritten) => {
+            console.log(`wrote ${ec2RecordsWritten} EC2 records to db table`);
+        })
+        // RDS instances
+        .then(() => clearAndFetchRDSRecords())
+        .then((rdsInstances) => writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, rdsInstances))
+        .then((rdsRecordsWritten) => {
+            console.log(`wrote ${rdsRecordsWritten} RDS records to db table`);
+        })
+        // Query AWS for EBS volumes
+        .then(() => clearAndFetchEBSVolumes())
+        .then((ebsVolumes) => {
+            console.log(`fetched ${ebsVolumes.length} EBS records`);
+            return writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, ebsVolumes);
+        })
+        .then((recordsWritten) => {
+            console.log(`wrote ${recordsWritten} EBS records to db table`);
+            return clearAndFetchS3Records();
+        })
         .then((buckets) => {
             console.log(`${buckets.length} buckets fetched from S3`);
             return writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, buckets);
