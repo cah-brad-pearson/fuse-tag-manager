8896f1aaf59d6635ad1adf0c15cbd08965da27b3
Brad Pearson <bradley.pearson@cardinalhealth.com>
2021-01-06 09:19:02 -0500

    uncommenting a large block of code that I forgot about

--- aws-importer/importer.js
+++ aws-importer/importer.js
@@ -12,39 +12,38 @@ AWS.config.update({ region: "us-east-1" });
 
 const importResources = (pcfEnvironments) =>
     new Promise((resolve, reject) => {
-        // clearAndFetchEc2Instances()
-        //     // EC2 instances
-        //     .then((ec2Instances) => writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, ec2Instances))
-        //     .then((ec2RecordsWritten) => {
-        //         logger.info(`wrote ${ec2RecordsWritten} EC2 records to db table`);
-        //     })
-        //     // RDS instances
-        //     .then(() => clearAndFetchRDSRecords())
-        //     .then((rdsInstances) => writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, rdsInstances))
-        //     .then((rdsRecordsWritten) => {
-        //         logger.info(`wrote ${rdsRecordsWritten} RDS records to db table`);
-        //     })
-        //     // EBS volumes
-        //     .then(() => clearAndFetchEBSVolumes())
-        //     .then((ebsVolumes) => {
-        //         logger.info(`fetched ${ebsVolumes.length} EBS records`);
-        //     })
-        //     .then(() => writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, ebsVolumes))
-        //     .then((recordsWritten) => {
-        //         logger.info(`wrote ${recordsWritten} EBS records to db table`);
-        //     })
-        //     // S3 buckets
-        //     .then(() => clearAndFetchS3Records())
-        //     .then((buckets) => {
-        //         logger.info(`${buckets.length} buckets fetched from S3`);
-        //     })
-        //     .then(() => writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, buckets))
-        //     .then((recordsWritten) => {
-        //         logger.info(`wrote ${recordsWritten} S3 records to db table`);
-        //     })
-        // PCF orgs
-        //.then(() => clearAndFetchPCFOrgs(pcfEnvironments))
-        clearAndFetchPCFOrgs(pcfEnvironments)
+        clearAndFetchEc2Instances()
+            // EC2 instances
+            .then((ec2Instances) => writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, ec2Instances))
+            .then((ec2RecordsWritten) => {
+                logger.info(`wrote ${ec2RecordsWritten} EC2 records to db table`);
+            })
+            // RDS instances
+            .then(() => clearAndFetchRDSRecords())
+            .then((rdsInstances) => writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, rdsInstances))
+            .then((rdsRecordsWritten) => {
+                logger.info(`wrote ${rdsRecordsWritten} RDS records to db table`);
+            })
+            // EBS volumes
+            .then(() => clearAndFetchEBSVolumes())
+            .then((ebsVolumes) => {
+                logger.info(`fetched ${ebsVolumes.length} EBS records`);
+            })
+            .then(() => writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, ebsVolumes))
+            .then((recordsWritten) => {
+                logger.info(`wrote ${recordsWritten} EBS records to db table`);
+            })
+            // S3 buckets
+            .then(() => clearAndFetchS3Records())
+            .then((buckets) => {
+                logger.info(`${buckets.length} buckets fetched from S3`);
+            })
+            .then(() => writeRecordsToDynamoDB(CONSTANTS.TABLE_NAME, buckets))
+            .then((recordsWritten) => {
+                logger.info(`wrote ${recordsWritten} S3 records to db table`);
+            })
+            //PCF orgs
+            .then(() => clearAndFetchPCFOrgs(pcfEnvironments))
             .then((results) => {
                 let keysLength = Object.keys(results.orgs).length;
                 logger.info(`${keysLength} pcf orgs fetched`);
